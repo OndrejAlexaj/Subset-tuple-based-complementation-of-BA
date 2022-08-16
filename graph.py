edf939b69902e1dc3c8c54b884b0c7f90ca806e8
OndrejAlexaj <alexajondrej@gmail.com>
2022-08-16 16:21:18 +0200

    renamed instation of Digraph

--- graph.py
+++ graph.py
@@ -2,23 +2,23 @@ import graphviz
 from buchi_automaton import *
 
 def draw_graph(automaton):
-    states = graphviz.Digraph('BA')  
-    states.attr(rankdir='LR')
-    states.attr('node', shape = 'circle')
+    automaton_image = graphviz.Digraph('BA')  
+    automaton_image.attr(rankdir='LR')
+    automaton_image.attr('node', shape = 'circle')
 
-    for state in automaton.states:
+    for state in automaton.automaton_image:
         if state in automaton.accepting:
-            states.attr('node',rankdir='LR', size = '8,5', shape = 'doublecircle')
+            automaton_image.attr('node',rankdir='LR', size = '8,5', shape = 'doublecircle')
         else:
-            states.attr('node',rankdir='LR', size = '8,5', shape = 'circle')
+            automaton_image.attr('node',rankdir='LR', size = '8,5', shape = 'circle')
             
-        states.node(state)
+        automaton_image.node(state)
 
 
     for i in automaton.transition:
         state_1 = i[0]
         in_symbol = i[1]
         for state_2 in i[2]:
-            states.edge(state_1, state_2, label = in_symbol)
+            automaton_image.edge(state_1, state_2, label = in_symbol)
 
-    states.render(directory='automaton')
+    automaton_image.render(directory='automaton')
