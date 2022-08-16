eb9a3d6c5e6dea65e9cd5e3e895e89b4b085ae74
OndrejAlexaj <alexajondrej@gmail.com>
2022-08-16 17:08:20 +0200

    Drawing initial state first

--- graph.py
+++ graph.py
@@ -1,24 +1,33 @@
 import graphviz
 from buchi_automaton import *
 
-def draw_graph(automaton):
-    automaton_image = graphviz.Digraph('BA')  
-    automaton_image.attr(rankdir='LR')
-    automaton_image.attr('node', shape = 'circle')
+def draw_initial(automaton, automaton_image):
+    for state in automaton.initial:
+        automaton_image.node(state)
 
+def draw_states(automaton, automaton_image):
     for state in automaton.states:
         if state in automaton.accepting:
-            automaton_image.attr('node',rankdir='LR', size = '8,5', shape = 'doublecircle')
+            automaton_image.attr('node',rankdir='LR', shape = 'doublecircle')
         else:
-            automaton_image.attr('node',rankdir='LR', size = '8,5', shape = 'circle')
+            automaton_image.attr('node',rankdir='LR', shape = 'circle')
             
         automaton_image.node(state)
 
-
+def draw_edges(automaton, automaton_image):
     for i in automaton.transition:
         state_1 = i[0]
         in_symbol = i[1]
         for state_2 in i[2]:
             automaton_image.edge(state_1, state_2, label = in_symbol)
+
+def draw_graph(automaton):
+    automaton_image = graphviz.Digraph('BA')  
+    automaton_image.attr(rankdir='LR')
+    automaton_image.attr('node', shape = 'circle')
+
+    draw_initial(automaton, automaton_image)  
+    draw_states(automaton, automaton_image)
+    draw_edges(automaton, automaton_image)
 
     automaton_image.render(directory='automaton')
