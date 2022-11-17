0205b5d5f2452f71b5e0c928f420a8bf26a4c36d
OndrejAlexaj <alexajondrej@gmail.com>
2022-11-17 23:57:24 +0100

    Lets go test

--- test.sh
+++ test.sh
@@ -5,31 +5,13 @@ total=0
 i=0
 
 for FILE in binary-encoding/*; do
-	if [$i -eq 1000]; then
-		break
+	if cat $FILE | autcross 'python3 main.py %H >%O' 'autfilt --complement %H >%O | autfilt --sba' | grep -q 'error'; then
+		echo "FAIL"
+	else
+		echo "PASS"
 	fi
-	total=$((total+1))
-	export PATH=$PATH":/home/ondrejalexaj/usr/bin"
-	my=$(python3 main.py $FILE 0 1)
-	aut=$(autfilt "$FILE" --complement | awk -F: '$1 == "States" {print $2}' | tr -dc '0-9')
-	ranker=$(../ranker/src/ranker "$FILE" --best | awk -F: '$1 == "States" {print $2}' | tr -dc '0-9')
-	if [ "$my" -lt "$aut" ]; then
-		echo "My algo: $my"
-		echo "Autfilt: $aut"
-		echo ".............."
-		pocet_autfilt=$((pocet_autfilt+1))
-	fi
-	
-	if [ "$my" -lt "$ranker" ]; then
-		echo "My algo: $my"
-		echo "Ranker: $ranker"
-		echo ".............."
-		pocet_ranker=$((pocet_ranker+1))
-	fi
+	#export PATH=$PATH":/home/ondrejalexaj/usr/bin"
+	echo "-----------------------------------"
+	#aut=$(autfilt "$FILE" --complement | awk -F: '$1 == "States" {print $2}' | tr -dc '0-9')
+	#ranker=$(../ranker/src/ranker "$FILE" --best | awk -F: '$1 == "States" {print $2}' | tr -dc '0-9')
 done
-
-echo "------------------------------"
-echo "			END					"
-echo "------------------------------"
-echo "Better than autfilt in $pocet_autfilt cases of total $total"
-echo "Better than ranker in $pocet_ranker cases of total $total"
